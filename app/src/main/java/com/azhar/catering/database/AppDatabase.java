05eaf0af7246f57e0af381615782bfa95385e70f
latifna <latifnuranhari@gmail.com>
2024-07-01 19:44:44 +0700

    first commit

---
+++ app/src/main/java/com/azhar/catering/database/AppDatabase.java
@@ -0,0 +1,14 @@
+package com.azhar.catering.database;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+import com.azhar.catering.database.dao.DatabaseDao;
+
+
+
+//untuk entitas model database
+@Database(entities = {DatabaseModel.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+    public abstract DatabaseDao databaseDao();
+}
---
+++ app/src/main/java/com/azhar/catering/login/LoginViewModel.java
@@ -0,0 +1,34 @@
+package com.azhar.catering.login;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.azhar.catering.database.DatabaseClient;
+import com.azhar.catering.database.DatabaseModel;
+import com.azhar.catering.database.dao.DatabaseDao;
+
+import java.util.List;
+
+
+public class LoginViewModel extends AndroidViewModel {
+
+    LiveData<List<DatabaseModel>> modelDatabase;
+    DatabaseDao databaseDao;
+
+    //untuk inisialisasi databaseDao
+    public LoginViewModel(@NonNull Application application) {
+        super(application);
+
+        databaseDao = DatabaseClient.getInstance(application).getAppDatabase().databaseDao();
+    }
+
+    //untuk cek login mengambil data dari database
+    public LiveData<List<DatabaseModel>> getDataUser(String username, String password) {
+        modelDatabase = databaseDao.getUserByName(username, password);
+        return modelDatabase;
+    }
+
+}
